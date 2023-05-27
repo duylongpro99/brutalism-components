91a5b82e4387fcb05ab04fa988f6e0e813ef59f5
1dayin2 <ripad3011@gmail.com>
2023-05-27 23:00:05 +0700

    chore: decorate file index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,12 @@
 /* @refresh reload */
-import { render } from "solid-js/web";
+import { MountableElement, render } from "solid-js/web";
 
-render(() => <div>Brutalism</div>, document.getElementById("root")!);
+const root = document.getElementById("root");
+
+if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
+  throw new Error(
+    "Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got mispelled?"
+  );
+}
+
+render(() => <div>Brutalism</div>, root as MountableElement);
